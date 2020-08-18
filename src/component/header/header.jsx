c2abe2dc4b09974079627766bf0a5dfde15a31a9
Frode <frode@sjovatsen.net>
2020-08-18 14:25:01 +0200

    updated teams link

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
                 </h1>
             </div>
             <div className="menuContainer">
-                <a href="https://teams.microsoft.com/_#/school/files/Oppl%C3%A6ring?threadId=19%3A4b450cc244494402bd9181752ca22441%40thread.skype&ctx=channel&context=Oppl%25C3%25A6ring&rootfolder=%252Fsites%252FVIGO-VigoIKSQlik2%252FDelte%2520dokumenter%252FOppl%25C3%25A6ring" target="_blank" className="menuLinks"
+                <a href="https://teams.microsoft.com/l/channel/19%3a4b450cc244494402bd9181752ca22441%40thread.skype/Oppl%25C3%25A6ring?groupId=e3192e0c-cfca-47ef-b0f0-6e7571dd3911&tenantId=08f3813c-9f29-482f-9aec-16ef7cbf477a" target="_blank" className="menuLinks"
                    title="Teams">Teams</a>
                 <a href="https://vigo-qlik-support.fintlabs.no/" target="_blank" className="menuLinks"
                    title="Support">Support</a>
