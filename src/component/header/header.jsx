257f0589aa9964029de69a6690eb948aa3a28102
Ola Brustad <ola.brustad@gmail.com>
2022-06-20 14:54:52 +0200

    Changed to mailto in support link

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
                 </h1>
             </div>
             <div className="menuContainer">
-                <a href="https://vigo-qlik-support.fintlabs.no/" target="_blank"
+                <a href="mailto:support@vigo-qlik.zendesk.com" target="_blank"
                    rel="noopener noreferrer" className="menuLinks"
                    title="Support">Support</a>
             </div>
